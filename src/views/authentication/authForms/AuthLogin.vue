0ce87e23cd34b8ef386d68408754e486472d0d7a
Pedro Lucas <lukkas.78@outlook.com>
2024-09-17 11:51:24 -0300

    feature: login screen and favicon

--- src/views/authentication/authForms/AuthLogin.vue
+++ src/views/authentication/authForms/AuthLogin.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import Google from '@/assets/images/auth/social-google.svg';
 import { useAuthStore } from '@/stores/auth';
 import { Form } from 'vee-validate';
 
@@ -24,23 +23,11 @@ function validate(values: any, { setErrors }: any) {
 </script>
 
 <template>
-  <v-btn block color="primary" variant="outlined" class="text-lightText googleBtn">
-    <img :src="Google" alt="google" />
-    <span class="ml-2">Sign in with Google</span></v-btn
-  >
-  <v-row>
-    <v-col class="d-flex align-center">
-      <v-divider class="custom-devider" />
-      <v-btn variant="outlined" class="orbtn" rounded="md" size="small">OR</v-btn>
-      <v-divider class="custom-devider" />
-    </v-col>
-  </v-row>
-  <h5 class="text-h5 text-center my-4 mb-8">Sign in with Email address</h5>
   <Form @submit="validate" class="mt-7 loginForm" v-slot="{ errors, isSubmitting }">
     <v-text-field
       v-model="username"
       :rules="emailRules"
-      label="Email Address / Username"
+      label="Usuário"
       class="mt-4 mb-8"
       required
       density="comfortable"
@@ -51,7 +38,7 @@ function validate(values: any, { setErrors }: any) {
     <v-text-field
       v-model="password"
       :rules="passwordRules"
-      label="Password"
+      label="Senha"
       required
       density="comfortable"
       variant="outlined"
@@ -67,27 +54,27 @@ function validate(values: any, { setErrors }: any) {
       <v-checkbox
         v-model="checkbox"
         :rules="[(v: any) => !!v || 'You must agree to continue!']"
-        label="Remember me?"
+        label="Lembrar de mim"
         required
         color="primary"
         class="ms-n2"
         hide-details
       ></v-checkbox>
       <div class="ml-auto">
-        <a href="javascript:void(0)" class="text-primary text-decoration-none">Forgot password?</a>
+        <a href="javascript:void(0)" class="text-primary text-decoration-none">Esqueceu sua senha?</a>
       </div>
     </div>
-    <v-btn color="secondary" :loading="isSubmitting" block class="mt-2" variant="flat" size="large" :disabled="valid" type="submit">
-      Sign In</v-btn
+    <v-btn color="primary" :loading="isSubmitting" block class="mt-2" variant="flat" size="large" :disabled="valid" type="submit">
+      Entrar</v-btn
     >
     <div v-if="errors.apiError" class="mt-2">
       <v-alert color="error">{{ errors.apiError }}</v-alert>
     </div>
   </Form>
-  <div class="mt-5 text-right">
+  <!-- <div class="mt-5 text-right">
     <v-divider />
     <v-btn variant="plain" to="/auth/register" class="mt-2 text-capitalize mr-n2">Don't Have an account?</v-btn>
-  </div>
+  </div> -->
 </template>
 <style lang="scss">
 .custom-devider {
